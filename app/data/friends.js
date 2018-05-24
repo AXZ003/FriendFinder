a9b670d5a3fe72ee9480d30f0f4c09af60351661
Andrea <Andrea@Macintosh.local>
2018-05-24 18:31:59 -0500

    Added to survey.html

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = 
+    {
+      name: "Andrea",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [
+          4,
+          3,
+          4,
+          1,
+          2,
+          1,
+          3,
+          3,
+          5,
+          5
+      ]
+      
+    
+    };
+  
+  
+
+
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
